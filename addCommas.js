58a30a6515ab2163fc98b6bd0a5d70835b26d5a5
Rijo George <rijogeorge@rijos-mbp-2.lan>
2022-03-05 18:13:50 -0800

    Part 2: addCommas done

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,23 @@
-function addCommas() {}
+function addCommas(num) {
+    const value = Math.abs(num);
+    const stringNumFraction = value.toString();
+    const intFractArray = stringNumFraction.split(".");
+    let stringNum = intFractArray[0];
+    let commaIndex = stringNum.length - 3;
+    let commaCount  = Math.floor(stringNum.length / 3);
+    while (commaCount>0) {
+        stringNum = stringNum.slice(0,commaIndex)+","+stringNum.slice(commaIndex);
+        commaIndex -= 3
+        commaCount--;
+    }
+    if (num < 0) {
+        stringNum = "-"+stringNum;
+    }
+    // attach if there is a fractional part
+    if(intFractArray.length>1) {
+        stringNum = stringNum+"."+intFractArray[1]
+    }
+    return stringNum
+}
 
 module.exports = addCommas;
--- addCommas.test.js
+++ addCommas.test.js
@@ -4,4 +4,37 @@ describe("#addCommas", () => {
   test("it is a function", () => {
     expect(typeof addCommas).toBe("function");
   });
+
+  it("1234 returns 1,234", () => {
+    expect(addCommas(1234)).toBe("1,234")
+  });
+
+  it("1000000 returns 1,000,000", () => {
+    expect(addCommas(1000000)).toBe("1,000,000")
+  });
+
+  it("9876543210 returns 9,876,543,210", () => {
+    expect(addCommas(9876543210)).toBe("9,876,543,210")
+  });
+
+  it("6 returns 6", () => {
+    expect(addCommas(6)).toBe("6")
+  });
+
+  it("-10 returns -10", () => {
+    expect(addCommas(-10)).toBe("-10")
+  });
+
+  it("-5678 returns -5,678", () => {
+    expect(addCommas(-5678)).toBe("-5,678")
+  });
+
+  it("12345.678 returns 12,345.678", () => {
+    expect(addCommas(12345.678)).toBe("12,345.678")
+  });
+
+  it("12345.678 returns 12,345.678", () => {
+    expect(addCommas(-3141592.65)).toBe("-3,141,592.65")
+  });
+
 });
